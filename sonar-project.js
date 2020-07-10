e6865cce323e13b8fc5520dde9e6f933d6c4a50f
Prakash Tomar <prakash_tomar@outlook.com>
2020-07-10 17:51:34 +0530

    Create sonar-project.js

---
+++ sonar-project.js
@@ -0,0 +1,9 @@
+  
+const sonarqubeScanner = require('sonarqube-scanner');
+     sonarqubeScanner({
+       serverUrl: 'http://3.7.34.19:9000',
+       options : {
+       'sonar.sources': '.',
+       //'sonar.inclusions' : '.' // Entry point of your code
+       }
+     }, () => {});
