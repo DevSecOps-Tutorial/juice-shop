1e30b2d236120bb438a44959bbeb3e76f9bc7283
JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>
2020-07-10 12:26:45 +0000

    Auto-fix linting issues
    Signed-off-by: JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>

--- sonar.js
+++ sonar.js
@@ -1,9 +1,9 @@
-  
-const sonarqubeScanner = require('sonarqube-scanner');
-     sonarqubeScanner({
-       serverUrl: 'http://3.7.34.19:9000',
-       options : {
-       'sonar.sources': '.',
-       //'sonar.inclusions' : '.' // Entry point of your code
-       }
-     }, () => {});
+
+const sonarqubeScanner = require('sonarqube-scanner')
+sonarqubeScanner({
+  serverUrl: 'http://3.7.34.19:9000',
+  options: {
+    'sonar.sources': '.'
+    // 'sonar.inclusions' : '.' // Entry point of your code
+  }
+}, () => {})
